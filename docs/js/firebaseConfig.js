5c971e99656502d6f0122b5b9e1768d98642478b
FerminDiaz98 <fermindiazpedrero@gmail.com>
2025-05-08 20:29:41 -0400

    googlelogin

---
+++ docs/js/firebaseConfig.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBh3MXUolXMzhofdO9-eEE6IiH2uOH7Slg",
+  authDomain: "testfirebasefanview.firebaseapp.com",
+  databaseURL: "https://testfirebasefanview-default-rtdb.firebaseio.com",
+  projectId: "testfirebasefanview",
+  storageBucket: "testfirebasefanview.firebasestorage.app",
+  messagingSenderId: "299143604685",
+  appId: "1:299143604685:web:4f32e72b0b176fdffb7c84"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
+
+
+document.getElementById("handleGoogle").addEventListener("click", function() {
+    console.log("test")
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider)
+  });
