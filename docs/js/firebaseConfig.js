2af2d7cb63544b956f14d9817a064b08c68e85c7
FerminDiaz98 <fermindiazpedrero@gmail.com>
2025-05-08 20:42:42 -0400

    firebaseConfig fix

--- docs/js/firebaseConfig.js
+++ docs/js/firebaseConfig.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
